e12f0fa57003ab02b54fab346b8dfff2e24af4c5
daniele capo <capo.daniele@gmail.com>
2012-05-13 11:24:14 +0200

    cambiamenti minori a sfd.clj

--- src/fonduta/sfd.clj
+++ src/fonduta/sfd.clj
@@ -1,13 +1,13 @@
 (ns fonduta.sfd
   (:require [clojure.string :as string]))
 
-(defn entry [k v]
+(defn- entry [k v]
   (str (name k) ": " v "\n"))
 
-(defn level [n s]
+(defn- level [n s]
   (if (= n 0) s (level (- n 1) (str " " s))))
 
-(defn segment [s]
+(defn- segment [s]
   (let [cmd (name (first s))]
     (str (string/join " " (rest s))
          " " cmd " "(if (= cmd "l") 1 0)
@@ -17,12 +17,12 @@
   (str (segment (first segments))
        (string/join (map (fn [s] (level 1 (segment s))) (rest segments)))))
        
-(defn contours [curves]
+(defn- contours [curves]
   (str "Fore\nSplineSet\n"
        (string/join (map contour curves))
        "EndSplineSet\n"))
 
-(defn glyph [[glyph-name code advance & curves]]
+(defn- glyph [[glyph-name code advance & curves]]
   (str (entry :StartChar (name glyph-name))
        (entry :Encoding (string/join " " code))
        (entry :Width (first advance))
@@ -30,7 +30,7 @@
        (contours curves)
        "EndChar\n"))
 
-(defn header [head]
+(defn- header [head]
   (reduce (fn [a b] (str a (apply entry b))) "" (rest head)))
 
 (defn font [[head & glyphs]]
@@ -41,20 +41,3 @@
        "EndChars\n"
        "EndSplineFont"))
 
-
-;; (spit "/Users/daniele/Desktop/abc2.sfd" (font
-;; 	      [[:header 
-;; 	      [:SplineFontDB 3.0]
-;; 	      [:FontName "abc"]
-;; 	      [:FullName "abc"]
-;; 	      [:FamilyName "abc"]
-;; 	      [:Ascent 800]
-;; 	      [:Descent 200]
-;; 	      [:Encoding "unicode"]
-;; 	      [:NameList "Adobe Glyph List"]]
-;; 	      [:o [111 111 0] [300 0]
-;; 	      [[:m 0 0]
-;; 	      [:l 100 0]
-;; 	      [:l 100 100]
-;; 	      [:l 0 100]
-;; 	      [:l 0 0]]]]))
