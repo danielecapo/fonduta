370ce1949c9343b16ae8c4b8da03482bcedb3536
daniele capo <capo.daniele@gmail.com>
2012-05-30 10:18:06 +0200

    fixed project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject fonduta "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject fonduta "0.0.1-SNAPSHOT"
+  :description "A library to make experiments with fonts"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  ;; [quil "1.2.0"]
-                 [seesaw "1.4.0"]
-                 [self/ufo "1.0.0"]]
+                 [seesaw "1.4.0"]]
+;;                 [self/ufo "1.0.0"]]
   :dev-dependencies [[swank-clojure "1.4.2"]])
